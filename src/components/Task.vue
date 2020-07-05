418d676bf6a9a0e42f4a6ae36b65c86bebff05cd
ura148 <yuki.ura10@gmail.com>
2020-07-06 00:01:29 +0900

    add：Task(List)の中にTaskを表示できるように設定

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -5,20 +5,36 @@
       <!-- 初期はVーmodelで"newTodoName"を入れているが空白、故にタスク作った後に空白にしないとミスが起こる -->
       <input type="text" v-model="newTodoName">
       <input type="date" v-model="deadline">
+      <!-- <button type="submit" v-on:click="createList()">タスク作成</button> -->
       <button type="submit" v-on:click="createTodo()">タスク作成</button>
     </div>
     <ul>
-      <li><button type="submit" v-on:click="showTodoType = 'all'">すべて</button></li>
-      <li><button type="submit" v-on:click="showTodoType = 'active'">未完タスク一覧</button></li>
-      <li><button type="submit" v-on:click="showTodoType = 'complete'">完了タスク一覧</button></li>
+      <li><button class="btn" type="submit" v-on:click="showTodoType = 'all'">すべて</button></li>
+      <li><button class="btn" type="submit" v-on:click="showTodoType = 'active'">未完タスク一覧</button></li>
+      <li><button class="btn" type="submit" v-on:click="showTodoType = 'complete'">完了タスク一覧</button></li>
     </ul>
+
+
     <!-- todoの一覧表示 -->
     <p>{{ taskNumber + "個のタスク" }}</p>
+
     <ul v-for="(todo, key) in filteredTodos" :key="todo.id">
       <li class="card">
-        <input type="checkbox" v-model="todo.isComplete" v-on:click="updateIsCompleteTodo(todo, key)">
+        <!-- <input type="checkbox" v-model="todo.isComplete" v-on:click="updateIsCompleteTodo(todo, key)"> -->
         <p>{{ todo.name }}</p>
         <p>{{ todo.date }}</p>
+
+        <!-- subTodoを表示 -->
+        <div class="subcard" v-for="(subtodo, subkey) in todo.subTasks" :key="subtodo.id">
+          <input type="checkbox" v-model="todo.isComplete" v-on:click="updateIsCompleteTodo(subodo, subkey)">
+          <p>{{ subtodo.subName }}</p>
+        </div>
+
+        <div>
+          <input type="text" v-model="newSubTodoName">
+          <input type="date" v-model="subDeadline">
+          <button type="submit" v-on:click="createSubTodo(key)">サブタスク作成</button>
+        </div>
       </li>
       <button type="submit" v-on:click="deleteTodo(key)">削除</button>
     </ul>
@@ -35,16 +51,19 @@ export default {
       database: null,
       todosRef: null,
       newTodoName: "",
+      newSubTodoName: "",
       deadline:"",
+      subDeadline: "",
       showTodoType: "all",
       todos: []
     };
   },
   created: function() {
-    this.database = firebase.database();
+    this.db = firebase.database();
     this.uid = firebase.auth().currentUser.uid;
     // refはreferenceでデータベースにある特定の項目を指し示すメソッド
-    this.todosRef = this.database.ref("todos/" + this.uid);
+    this.todosRef = this.db.ref("todos/" + this.uid);
+    this.test = this.db.ref("todos/" + this.uid + "/")
     // データに変更があると実行されるfunction
     var _this = this;
     this.todosRef.on('value', function(snapshot) {
@@ -55,7 +74,6 @@ export default {
     // フィルターの実装showTodoTypeが変更されると実行される
     filteredTodos: function () {
       if (this.showTodoType == 'all') {
-        console.log(this.todos);
         return this.todos;
       } else {
         var showComplete = false;
@@ -63,15 +81,12 @@ export default {
           showComplete = true
         }
         var filterTodos = {};
-
         for (var key in this.todos) {
           var todo = this.todos[key];
           if (todo.isComplete == showComplete) {
             filterTodos[key] = todo;
-            console.log(filterTodos[key]);
           }
         }
-        console.log(filterTodos);
         return filterTodos;
       }
     },
@@ -79,52 +94,30 @@ export default {
       let count = 0;
       for (var key in this.todos) {
         var todo = this.todos[key];
+        // console.log(todo.id);
         if (this.showTodoType == 'all'){
-          console.log(key);
+          console.log(todo);
+          console.log(todo.subTasks);
           count += 1;
-          console.log(count);
+          for (var subkey in todo.subTasks) {
+            console.log(subkey);
+            var subtodo = todo.subTasks[subkey];
+            console.log(subtodo);
+            console.log(subtodo.subName);
+            count += 1;
+          }
         } else if (this.showTodoType == 'active'){
           if (todo.isComplete == false){
             count += 1;
-            console.log(count);
-          }    
+          }
         } else {
           if (todo.isComplete == true){
             count += 1;
           }
         }
-        // var todo = this.todos[key];
-        // if (todo.isComplete == showComplete) {
-        //   filterTodos[key] = todo;
-        //   console.log(filterTodos[key]);
-        // }
       }
       return count;
-
-      // var showComplete = false;
-      // if (this.showTodoType == 'complete') {
-      //   showComplete = true
-      // }
-      // var filterTodos = {};
-      // for (var key in this.todos) {
-      //   var todo = this.todos[key];
-      //   if (todo.isComplete == showComplete) {
-      //     filterTodos[key] = todo;
-      //     console.log(filterTodos[key]);
-      //   }
-      // }
-    },
-    // remaining: function() {
-		// 	var count = 0;
-		// 	var todos = this.todos;
-		// 	var length = todos.length;
-    //   console.log(length);
-			// for(var i = 0; i < length; i++) {
-			// 	if(!todos[i].done) {
-			// 		count++;
-			// 	}
-			// }
-			// return count;
+    }
   },
   methods: {
     // DBのtodos/[uid]/以下にデータを格納していく
@@ -137,7 +130,16 @@ export default {
       })
       this.newTodoName = "";
     },
-    // 完了・未完了の値の更新
+    createSubTodo: function(key) {
+      if (this.newSubTodoName == "") { return; }
+      this.todosRef.child(key).child("/subTasks").push({
+        subName: this.newSubTodoName,
+        isComplete: false,
+        subDate: this.subDeadline,
+        pairingId: key,
+      })
+      this.newSubTodoName = "";
+    },
     updateIsCompleteTodo: function(todo, key) {
       todo.isComplete = !todo.isComplete;
       // DB内のデータを更新する
@@ -155,5 +157,12 @@ export default {
 </script>
 
 <style>
+.card {
+  background-color: #c8d2e3;
+}
+
+.subcard {
+  background-color: #a6b6d5;
+}
 
 </style>
